17266be9c9080fcbaf92a4c56cbd650d6bdf9a23
minghong.kang <minghong.kang@iluvatar.ai>
2022-03-07 10:53:04 +0800

    add demo

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -2,23 +2,23 @@ pipeline {
     agent {
         docker {
             image "node:16.13.1-alpine"
-            args "-v ${env.WORKSPACE}:/workspace"
+            args "-v /workspace:/workspace"
         }
     }
     stages {
         stage('Test1') {
             steps {
-                sh "pwd;ls -l;node --version;echo ${env.WORKSPACE}"
+                sh "pwd; ls -l; node --version; echo ${env.WORKSPACE}"
             }
         }
         stage('Test2') {
             steps {
-                sh "ls -l /workspace;node --version; touch /workspace/kenny.txt"
+                sh "ls -l /workspace; node --version; touch /workspace/kenny.txt"
             }
         }
         stage('Test3') {
             steps {
-                sh "ls -l /workspace;node --version"
+                sh "ls -l /workspace; node --version"
             }
         }
     }
