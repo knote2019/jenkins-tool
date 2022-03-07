1547c23a80717675a5fdc1f1d269a9d9244634bf
minghong.kang <minghong.kang@iluvatar.ai>
2022-03-07 10:04:40 +0800

    add demo

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -3,7 +3,17 @@ pipeline {
         docker { image 'node:16.13.1-alpine' }
     }
     stages {
-        stage('Test') {
+        stage('Test1') {
+            steps {
+                sh 'node --version'
+            }
+        }
+        stage('Test2') {
+            steps {
+                sh 'node --version'
+            }
+        }
+        stage('Test3') {
             steps {
                 sh 'node --version'
             }
