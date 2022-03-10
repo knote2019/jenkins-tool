be69f0ff72d0c443fb989a3607338ade60ad544c
minghong.kang <minghong.kang@iluvatar.ai>
2022-03-10 10:57:48 +0800

    fix demo

--- src/pipeline2.groovy
+++ src/pipeline2.groovy
@@ -1,28 +1,72 @@
 pipeline {
-  agent {
-    kubernetes {
-      cloud 'kubernetes'
-      label 'mypod'
-      yaml """
-apiVersion: v1
-kind: Pod
-spec:
-  hostNetwork: true
-  containers:
-  - name: maven
-    image: maven:3.3.9-jdk-8-alpine
-    command: ['cat']
-    tty: true
-"""
-    }
-  }
-  stages {
-    stage('Run maven') {
-      steps {
-        container('maven') {
-          sh 'mvn -version'
+    agent none
+    stages {
+        stage('host1'){
+            agent { node { label "10.115.0.251" } }
+            options {
+                timestamps ()
+                ansiColor('xterm')
+            }
+            stages {
+                stage('host1 docker') {
+                    agent {
+                        docker {
+                            image "node:16.13.1-alpine"
+                        }
+                    }
+                    stages {
+                        stage('Test1') {
+                            steps {
+                                sh "echo 'Test1'"
+                            }
+                        }
+                        stage('Test2') {
+                            steps {
+                                sh "echo 'Test2'"
+                            }
+                        }
+                        stage('Test3') {
+                            steps {
+                                sh "echo 'Test3'"
+                            }
+                        }
+                    }
+                }
+            }
         }
-      }
+
+        stage('host2'){
+            agent { node { label "10.115.0.251" } }
+            options {
+                timestamps ()
+                ansiColor('xterm')
+            }
+            stages {
+                stage('host2 docker') {
+                    agent {
+                        docker {
+                            image "node:16.13.1-alpine"
+                        }
+                    }
+                    stages {
+                        stage('Test1') {
+                            steps {
+                                sh "echo 'Test1'"
+                            }
+                        }
+                        stage('Test2') {
+                            steps {
+                                sh "echo 'Test2'"
+                            }
+                        }
+                        stage('Test3') {
+                            steps {
+                                sh "echo 'Test3'"
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-  }
 }
