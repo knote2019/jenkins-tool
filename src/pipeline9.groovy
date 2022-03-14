cfe7c55328928aa44abbd4bfecc2c4f096bb3479
minghong.kang <minghong.kang@iluvatar.ai>
2022-03-14 15:37:07 +0800

    add allure

--- src/pipeline9.groovy
+++ src/pipeline9.groovy
@@ -35,7 +35,7 @@ spec:
       steps {
         container('maven') {
           sh 'ls -l /stores'
-          sh 'mkdir /kenny; chmod 666 /kenny'
+          sh 'mkdir /kenny; chmod 777 /kenny'
           sh 'ls -l /'
           allure([
             includeProperties: false,
