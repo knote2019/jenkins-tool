f2feeda006225b910eda929023560372488b3d82
minghong.kang <minghong.kang@iluvatar.ai>
2022-03-17 12:33:52 +0800

    fix demo

--- src/pipeline9.groovy
+++ src/pipeline9.groovy
@@ -1,49 +1,48 @@
 pipeline {
-  agent {
-    kubernetes {
-      cloud 'kubernetes'
-      label 'mypod'
-      yaml """
+    agent {
+        kubernetes {
+            cloud 'kubernetes'
+            label 'mypod'
+            yaml """
 apiVersion: v1
 kind: Pod
 spec:
-  hostNetwork: true
-  containers:
-  - name: maven
-    image: 10.150.9.98:80/devops_tools/jenkins-agent:master
-    command: ['cat']
-    tty: true
-    volumeMounts:
+    hostNetwork: true
+    containers:
+    - name: maven
+        image: 10.150.9.98:80/devops_tools/jenkins-agent:master
+        command: ['cat']
+        tty: true
+        volumeMounts:
+        - name: nfs-stores
+          mountPath: /stores
+          readOnly: true
+    volumes:
     - name: nfs-stores
-      mountPath: /stores
-      readOnly: true
-  volumes:
-  - name: nfs-stores
-    hostPath:
+      hostPath:
       path: /stores
 """
-    }
-  }
-
-  options {
-    timestamps()
-    skipDefaultCheckout()
-  }
-
-  stages {
-    stage('Run maven') {
-      steps {
-        container('maven') {
-          sh 'ls -l /stores'
-          sh 'mkdir /kenny; chmod 777 /kenny'
-          sh 'ls -l /'
-          allure([
-            includeProperties: false,
-            jdk: '',
-            results: [[path: '/']]
-          ])
         }
-      }
     }
-  }
+
+    options {
+        timestamps()
+        skipDefaultCheckout()
+    }
+
+    stages {
+        stage('Run maven') {
+            steps {
+                container('maven') {
+                    stages {
+                        stage('host1 host') {
+                            steps {
+                                sh 'ls -l /stores'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
